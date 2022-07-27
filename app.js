ce45c317bc2e6b646289d187f29feeecf3b5e3dd
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2022-07-27 21:48:25 +0200

    update for scalability

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let num = 0;
 let numHolder = document.getElementById('numHolder');
+let cats = document.querySelectorAll('.cat');
 
-let cat = document.getElementById('cat');
-cat.addEventListener('click', ()=>{
-   num += 1; 
-   numHolder.innerHTML = ''+num;
+cats.forEach((cat) => {
+    let num = 0;
+    cat.children[1].innerHTML = cat.children[2].alt;
+    cat.addEventListener('click', ()=>{
+        num += 1;
+        cat.children[0].innerHTML = ''+num;
+    })
 })
-
-
