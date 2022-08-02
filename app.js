550ab414b42c41b60b52ffc0e6d4e87fdbd5f70a
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2022-08-02 19:26:34 +0200

    refactor code to MV* architecture

--- app.js
+++ app.js
@@ -1,22 +1,95 @@
-let numHolder = document.getElementById('numHolder');
-let cats = document.querySelectorAll('.cat');
-let catNames = document.querySelectorAll('.catName');
+let model = [
+    cat1 = {
+        name: 'koko kitty',
+        clickCount: 0,
+        fileName: 'cat.jpg'
+    },
+    cat2 = {
+        name: 'lolo kitty',
+        clickCount: 0,
+        fileName: 'cat2.jpg'
+    },
+    cat3 = {
+        name: 'soso kitty',
+        clickCount: 0,
+        fileName: 'cat3.jpg'
+    },
+    cat4 = {
+        name: 'zozo kitty',
+        clickCount: 0,
+        fileName: 'cat4.jpg'
+    },
+    cat5 = {
+        name: 'roro kitty',
+        clickCount: 0,
+        fileName: 'cat5.jpg'
+    }
+]
 
-cats.forEach((cat) => {
-    let num = 0;
-    cat.children[1].innerHTML = cat.children[2].alt;
-    cat.addEventListener('click', ()=>{
-        num += 1;
-        cat.children[0].innerHTML = ''+num;
-    })
-})
+let view1 = {
+    addEventListeners: () => {
+        const cats = document.querySelectorAll('.cat');
+        const image = document.querySelector('#image');
 
-catNames.forEach((cat) =>{
-    cat.addEventListener('click', ()=>{
-        if(document.querySelector('.show')){
-            let showedItem = document.querySelector('.show');
-            showedItem.classList.toggle('show');
+        image.addEventListener('click', octopus.incrementCount);
+
+        for (let i=0; i<cats.length; i++) {
+              cats[i].addEventListener('click', octopus.preview)
         }
-        cat.nextElementSibling.classList.add('show');
-    })
-})
+    },
+    render: () => {
+        for ( let i=0; i<octopus.getDataCount(); i++){
+            const button = document.createElement('button');
+            button.setAttribute('class', 'cat');
+            button.setAttribute('id', i);
+            const name = model[i].name;
+            const textNode = document.createTextNode(name);
+            button.appendChild(textNode);
+            const buttonContainer = document.getElementById('buttonContainer');
+            buttonContainer.appendChild(button);
+        }
+        view1.addEventListeners();
+    }
+}
+
+let view2 = {
+    getImage: () => {
+        return document.querySelector('#preview');
+    },
+    render: (cat) => {
+        document.querySelector('#image').setAttribute('src', './images/'+cat.fileName);
+        document.querySelector('#catName').innerHTML = cat.name;
+        document.querySelector('#count').innerHTML = cat.clickCount;
+    }
+}
+
+let octopus = {
+    render: (cat) => {
+        view1.render();
+        view2.render(cat);
+    },
+    getDataCount: () => {
+        return model.length;
+    },
+    getCatNames: () => {
+        return model.map((cat) => {
+            return cat.name;
+        })
+    },
+    preview: (event) => {
+        const catName = event.path[0].innerHTML;
+        model.forEach((cat) => {
+            if (cat.name === catName) {
+                view2.render(cat);
+            }
+        });
+    },
+    incrementCount: (event) => {
+        let cat = model.find(element=>element.fileName===event.path[0].src.split('images/')[1]);
+        cat.clickCount += 1;
+        view2.render(cat);
+        // cat.clickCount += 1;
+    }
+}
+
+octopus.render(model[0]);
